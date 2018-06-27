9ee4e1b06a906fe26cbe1910c60e0867485310af
Khuezy <khuezy.nguyen@gmail.com>
2018-06-27 21:46:53 +0000

    wip

--- src/routes/garage.js
+++ src/routes/garage.js
@@ -4,12 +4,15 @@ const myq = require('../lib/myq')
 routes.get('/open', async  (req, res) => {
 	const result = await myq.login()
         const devices = await myq.getDevices([17])
-	console.log(devices)
 	const door = devices.devices[0]
-
-	const response = await myq.setDoorState(door.id, 1)
-	
-	console.log(response)
+	if (!door) {
+		res.statusCode(500)
+		res.send({success: false, message: 'Garage not found'})
+	}
+	if (door.doorState !== 1) {
+		const response = await myq.setDoorState(door.id, 1)
+		console.log(response)
+	}
 	res.send('opened?')
 })
 
@@ -17,10 +20,11 @@ routes.get('/close', async  (req, res) => {
 	const result = await myq.login()
         const devices = await myq.getDevices([17])
 	const door = devices.devices[0]
-	console.log('door: ', door)
-	const response = await myq.setDoorState(door.id, 0)
-	
-	console.log(response)
+
+	if (door.doorState !== 2) {
+		const response = await myq.setDoorState(door.id, 0)
+		console.log(response)
+	}
 	res.send('closed?')
 })
 
